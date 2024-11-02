9b1ab7577f99fb5c08614218a49167db101e7377
weitao1209 <58452075+weitao1209@users.noreply.github.com>
2024-11-02 10:42:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -44,6 +44,19 @@ mt7981_sdk_get()
 	 git clone https://github.com/gl-inet-builder/openwrt-sdk-mt7981.git  openwrt-sdk
 }
 
+rockchip_sdk_get()
+{
+  	wget -q -O openwrt-sdk.tar.xz https://downloads.immortalwrt.org/releases/23.05.4/targets/rockchip/armv8/immortalwrt-sdk-23.05.4-rockchip-armv8_gcc-12.3.0_musl.Linux-x86_64.tar.xz
+	mkdir -p ${WORKDIR}/openwrt-sdk
+	tar -Jxf openwrt-sdk.tar.xz -C ${WORKDIR}/openwrt-sdk --strip=1
+	echo src-git packages https://git.openwrt.org/feed/packages.git^78bcd00c13587571b5c79ed2fc3363aa674aaef7 >${WORKDIR}/openwrt-sdk/feeds.conf.default
+	echo src-git routing https://git.openwrt.org/feed/routing.git^a0d61bddb3ce4ca54bd76af86c28f58feb6cc044 >>${WORKDIR}/openwrt-sdk/feeds.conf.default
+	echo src-git telephony https://git.openwrt.org/feed/telephony.git^0183c1adda0e7581698b0ea4bff7c08379acf447 >>${WORKDIR}/openwrt-sdk/feeds.conf.default
+	echo src-git luci https://git.openwrt.org/feed/routing.git^a0d61bddb3ce4ca54bd76af86c28f58feb6cc044 >>${WORKDIR}/openwrt-sdk/feeds.conf.default
+	
+	sed -i '246,258d' ${WORKDIR}/openwrt-sdk/include/package-ipkg.mk
+}
+
 
 case "$BOARD" in
 	"SF1200" |\
@@ -53,6 +66,10 @@ case "$BOARD" in
 	"AX1800" |\
 	"AXT1800" )
 		axt1800_sdk_get
+  	;;
+	"R5C" |\
+	"R5S" )
+		rockchip_sdk_get
 	;;
 	"MT3000" |\
 	"MT2500" )
